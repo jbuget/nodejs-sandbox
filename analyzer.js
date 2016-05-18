51455b6ba95db017f25a575b6a832ef0e1cdc380
Jérémy Buget <jbuget@octo.com>
2016-05-18 14:02:43 +0200

    Add initializer

---
+++ analyzer.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+
+function loadData(inputFile) {
+
+    return new Promise(function(resolve, reject) {
+
+        fs.readFile(inputFile, 'utf8', (err, data) => {
+
+            if (err) return reject(err);
+            return resolve(data);
+        });
+    });
+}
+
+function parseLines(data) {
+
+    return new Promise(function(resolve, reject) {
+
+        const lines = data.toString().split("\n");
+        const pattern = new RegExp(/([\W\d]+) \[(\w+)\] (\w+#\w+) - (START|END)/);
+        const results = [];
+
+        for (var i in lines) {
+            var line = lines[i],
+                matches = line.match(pattern);
+            if (matches) {
+                var method = matches[3],
+                    phase = matches[4];
+
+                results.push(method + '-' + phase);
+            }
+        }
+        results.push('');
+
+        return resolve(results);
+    });
+}
+
+function generateReport(outputFile, results) {
+
+    return new Promise(function(resolve, reject) {
+
+        fs.unlink(outputFile, () => {
+
+            fs.appendFile(outputFile, results.join('\r\n'), (err) => {
+
+                if (err) throw err;
+
+                console.log('Report generated with success!');
+                return resolve(true);
+            });
+        });
+    });
+}
+
+loadData('input.log')
+    .then(data => parseLines(data))
+    .then(results => generateReport('output.log', results))
